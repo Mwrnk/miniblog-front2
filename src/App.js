5ab377aa8389d903dd5d5877a7b2c9ddbae4cf68
souzz2 <108484845+souzz2@users.noreply.github.com>
2024-09-10 15:04:00 -0300

    Criação da base dos Inputs do site
    Aqui foram criados a base dos inputs, são eles o titulo, conteúdo e link. Além disso foi criado também o botão Adicionar"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Header } from './Components/Header';
-
+import { Inputs } from "./Components/Inputs";
 
 function App() {
   return(
   <div>
     <Header></Header>
+    <Inputs></Inputs>
   </div>
   )
 }
---
+++ src/Components/Inputs.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+const Formu = styled.form`
+    display: flex;
+    unicode-bidi: isolate;
+    flex-direction: column;
+`
+export class Inputs extends React.Component {
+    state={
+        postagens: [
+        ],
+        titulo: "",
+        conteudo:"",
+        link: ""
+    }
+
+    onChangeTitulo = (event) => {
+        this.setState({ titulo: event.target.value });
+      };
+    
+      onChangeConteudo = (event) => {
+        this.setState({ conteudo: event.target.value });
+      };
+
+      onChangeLink = (event) => {
+        this.setState({ link: event.target.value });
+      };
+
+    adicionarPostagem = (event) => {
+        let titulo = this.state.titulo
+        let conteudo = this.state.conteudo
+        let link = this.state.link
+
+    }
+  render() {
+    return (
+      <>
+        <section>
+            <Formu>
+                <input type='text' placeholder="Titulo da Postagem" onChange={this.onChangeTitulo} value={this.state.titulo}></input>
+                <textarea type='text' placeholder="Conteudo" onChange={this.onChangeConteudo} value={this.state.conteudo}></textarea>
+                <input type='text' placeholder="Link" onChange={this.onChangeLink} value={this.state.link}></input>
+            </Formu>
+        </section>
+        <div>
+            <button onClick={this.adicionarPostagem}>Adicionar</button>
+        </div>
+      </>   
+    );
+  }
+}
