ae5130d0a2ea740f3a1370ef5daa5bc71a5c876c
souzz2 <108484845+souzz2@users.noreply.github.com>
2024-09-17 12:39:37 -0300

    Criação do botão de "Ler Mais" e o Modal
    Foram feitas as criações das bases do botão de ler mais e do modal

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,5 +1,6 @@
-import React from "react";
+
 import styled from "styled-components";
+import React, { useState } from "react";
 
 const Button = styled.button`
   background-color: #1ab85e;
@@ -47,16 +48,83 @@ const PostItem = styled.div`
   margin-bottom: 10px;
 `;
 
+const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const ModalContent = styled.div`
+   background: white;
+  padding: 20px;
+  border-radius: 8px;
+  width: 80%;
+  max-width: 600px;
+  overflow-y: auto;
+  white-space: nowrap;
+`;
+
+const ModalText = styled.p`
+  margin: 0;
+  min-width: 100%; /* Garante que o texto ocupe pelo menos a largura do modal */
+`;
+
+const CloseButton = styled.button`
+  background-color: #f44336;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    background-color: #d32f2f;
+  }
+`;
+
+const PostText = styled.p`
+  max-height: 60px; /* Define uma altura máxima para o texto */
+  overflow: hidden; /* Esconde o texto que ultrapassa o max-height */
+  text-overflow: ellipsis; /* Adiciona reticências (...) no final do texto */
+  margin: 0;
+`;
+
 const Postagem = ({ titulo, conteudo, link, id, removerPostagem }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
+
   return (
-    <PostCard>
-      <PostImage src={link} alt={titulo} />
-      <PostContent>
-        <H2>{titulo}</H2>
-        <p>{conteudo}</p>
-        <Button onClick={() => removerPostagem(id)}>Remover</Button>
-      </PostContent>
-    </PostCard>
+    <>
+      <PostCard>
+        <PostImage src={link} alt={titulo} />
+        <PostContent>
+          <H2>{titulo}</H2>
+          <PostText>{conteudo.substring(0, 100)}...</PostText>
+          <Button onClick={handleOpenModal}>Ler mais</Button>
+          <Button onClick={() => removerPostagem(id)}>Remover</Button>
+        </PostContent>
+      </PostCard>
+
+      {modalOpen && (
+        <Modal>
+          <ModalContent>
+            <h2>{titulo}</h2>
+            <ModalText>{conteudo}</ModalText>
+            <CloseButton onClick={handleCloseModal}>Fechar</CloseButton>
+          </ModalContent>
+        </Modal>
+      )}
+    </>
   );
 };
 
