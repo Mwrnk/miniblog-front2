0e045cd1202dc68d483805d601d817c92929ff6a
Pedro Marazo <pedromarazo@outlook.com>
2024-09-19 19:49:12 -0300

    Removendo mais alguns arquivos desnecessários

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Header } from './Components/Header';
 import { Banner } from './Components/Banner';
 import { Inputs } from "./Components/PostForm";
-import { Posts } from "./Components/Post";
 
 function App() {
   return(
@@ -10,7 +9,6 @@ function App() {
     <Header></Header>
     <Banner></Banner>
     <Inputs></Inputs>
-   {/* <Posts></Posts> */}
   </div>
   )
 }
